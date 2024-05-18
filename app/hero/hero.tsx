7743e5492f670c3929f19c62cfa60c96509671a0
Eesha <eeshaulh@gmail.com>
2024-05-18 19:00:21 +0900

    started envelope base

--- app/hero/hero.tsx
+++ app/hero/hero.tsx
@@ -3,7 +3,7 @@
 import UnstyledLinkButton from '../../components/Button';
 import Image from 'next/image';
 import { Box, Grid, Typography, Stack} from '@mui/material';
-import Envelope from '../.././public/Envelope.svg'; // Adjust the path as necessary
+import rectangleEnvelope from '../.././public/rectangleEnvelope.svg'; // Adjust the path as necessary
 
 export default function Hero() {
     return (
@@ -19,14 +19,38 @@ export default function Hero() {
 
         
 
+        <div className="relative w-fit flex justify-center mt-16">
+        {/* MAIL LETTER PAPER */}
+        <div className="absolute inset-0 rounded-lg bg-white w-2/3 h-full mx-auto p-20 text-left text-sm" style={{ zIndex: 0 }}> 
+          <div className='font-semibold'>
+            <p>From: zkEmail.com</p>
+            <p>To: Developers</p>
+            <p className='pt-8'>Subject: prove any content for any email, sent or received </p>
+          </div>
+          <div>
+            <p>
+            we built a primitive that allows you to prove only what content you want from an email.  hide what you don’t want known.  + Prove who sent the email.
+            <br></br>
+            Excited to see what you build : )
+            <br></br>
+            <span className='font-semibold'>The Zk team</span>
+            <br></br>
+            Aayush, Sora, Saleel, Wataru, Aditya and Elo
 
-        <Box sx={{ marginTop: 'auto' }}>
+            </p>
+          </div>
+
+        </div>
+        {/* ACTUAL ENVELOPE */}
+        <Box sx={{ zIndex: 1 }}>
           <Image
-            src={Envelope}
-            alt='checkbox'
-            className='w-full h-full fill-white pt-24'
+            src={rectangleEnvelope}
+            alt='Envelope'
+            className='w-full h-full'
+            style={{ zIndex: 1 }}
           />
         </Box>
+      </div>
       </main>
     );
   }
