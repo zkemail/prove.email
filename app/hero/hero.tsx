2147970e110fbdb7d2987b83b92f10825c810b6a
Eesha <eeshaulh@gmail.com>
2024-05-18 21:14:36 +0900

    made the envelope get cut off from bottom, added top, but need better more frosted svg of a pentagon instead of triangle

--- app/hero/hero.tsx
+++ app/hero/hero.tsx
@@ -3,7 +3,8 @@
 import UnstyledLinkButton from '../../components/Button';
 import Image from 'next/image';
 import { Box, Stack } from '@mui/material';
-import rectangleEnvelope from '../.././public/rectangleEnvelope.svg';
+import rectangleEnvelope from '../.././public/rectangleEnvelope.svg'; // Adjust the path as necessary
+import triangleEnvelope from '../.././public/triangleEnvelope.svg'; // Adjust the path as necessary
 import LabelImportantIcon from '@mui/icons-material/LabelImportant';
 import { useState } from 'react';
 
@@ -15,20 +16,23 @@ export default function Hero() {
   };
 
   return (
-    <main className="p-16 w-full bg-[#F6F5F5] text-center align-content-center content-center">
-      <h1 className="text-black text-9xl">ZKEmail</h1>
-      <p className='pb-3'>Prove who sent an email & any of its contents. Anonymously. On or Offchain.</p>
-      <div className='content-center items-center flex justify-center'>
-        <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
-          <UnstyledLinkButton buttonLabel="Docs" filledIn={true} url='https://zkemail.gitbook.io/zk-email'>material ui button</UnstyledLinkButton>
-          <UnstyledLinkButton buttonLabel="Projects" filledIn={false}>material ui button</UnstyledLinkButton>
-        </Stack>
+    <main className="px-16 pt-16 w-full bg-[#F6F5F5] text-center align-content-center content-center">
+      <div className='z-50 relative'>
+        <h1 className="text-black text-9xl">ZKEmail</h1>
+        <p className='pb-3'>Prove who sent an email & any of its contents. Anonymously. On or Offchain.</p>
+        <div className='content-center items-center flex justify-center'>
+          <Stack spacing={2} direction="row" sx={{ paddingTop: "16px" }}>
+            <UnstyledLinkButton buttonLabel="Docs" filledIn={true} url='https://zkemail.gitbook.io/zk-email'>material ui button</UnstyledLinkButton>
+            <UnstyledLinkButton buttonLabel="Projects" filledIn={false}>material ui button</UnstyledLinkButton>
+          </Stack>
+        </div>
       </div>
+
 
       {/* ENVELOPE MAIL STUFF */}
       <div className="relative w-fit flex justify-center mt-16 cursor-pointer" onClick={handleToggle}>
         {/* MAIL LETTER PAPER */}
-        <div className="absolute inset-0 rounded-lg bg-white w-3/4 mx-auto p-5 sm:p-16 sm:p-20 text-left text-[8px] sm:text-[12px] md:text-[18px]" style={{ zIndex: 0 }}>
+        <div className="absolute inset-0 rounded-lg bg-white w-3/4 mx-auto p-5 sm:p-16 sm:p-20 text-left text-[8px] sm:text-[12px] md:text-[18px]" style={{ zIndex: 1 }}>
           <div className='font-semibold'>
             <div className='grid grid-cols-2'>
               <div>
@@ -61,11 +65,20 @@ export default function Hero() {
             </p>
           </div>
         </div>
-        {/* ACTUAL ENVELOPE */}
-        <Box className={`transition-transform duration-500 ${isEmailOpen ? 'transform translate-y-24' : ''}`} sx={{ zIndex: 1 }}>
+        {/* TRIANGLE PART OF ENVELOPE */}
+        <Box className={`absolute transition-transform duration-500 ${isEmailOpen ? 'transform translate-y-24' : ''}`} sx={{ zIndex: 0, top: '-50%', width: '100%', height: 'auto' }}>
+          <Image
+            src={triangleEnvelope}
+            alt='Triangle Envelope'
+            className='w-full h-full'
+            style={{ zIndex: 0 }}
+          />
+        </Box>
+        {/* RECTANGULAR ENVELOPE */}
+        <Box className={`transition-transform duration-500 ${isEmailOpen ? 'transform translate-y-24' : ''}`} sx={{ zIndex: 1, clipPath: 'inset(-50% 0 0 0)' }}>
           <Image
             src={rectangleEnvelope}
-            alt='Envelope'
+            alt='Rectangle Envelope'
             className='w-full h-full'
             style={{ zIndex: 1 }}
           />
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,104 @@
+// import Hero from './hero/hero'
+// import Accordion from '../components/Accordion';
+// import PopOut from '../components/PopOut';
+// import VideoCarousel from '../components/VideoCarousel';
+
+
+
+
+
+
+// const faqs = [
+//   {
+//     title: '01. Cryptography for People',
+//     contents: 'Cryptography is everywhere: every time you connect to a secure site...'
+//   },
+//   {
+//     title: '02. Prioritizing Privacy',
+//     contents: 'Authenticate themselves, make confidential transactions on the blockchain, and respect and preserve user privacy.'
+//   }
+// ];
+
+// const PopOutZKLibraryCards = [
+//   { label: "ZK Regex Library", url: "https://example.com/1" },
+//   { label: "halo2 benchmarking browser code", url: "https://example.com/2" },
+//   { label: "zk psi with grumpkin and hash to curve", url: "https://example.com/3" },
+//   { label: "zk strings (base64 + pack)", url: "https://example.com/3" },
+//   { label: "zk rsa", url: "https://example.com/3" },
+//   { label: "zk variable length sha256", url: "https://example.com/3" },
+// ];
+
+// let PopOutZKEmailLibraryCards = [
+//   { label: "zk-email", url: "https://example.com/1" },
+//   { label: "ZK Regex UI Tools", url: "https://example.com/2" },
+//   { label: "DKIM Archive", url: "https://example.com/3" },
+//   { label: "ZK JWTs", url: "https://example.com/3" },
+//   { label: "Regex Registry", url: "https://example.com/3" },
+//   { label: "Ether Email Auth", url: "https://example.com/3" },
+//   ];
+
+
+
+
+  
+// export default function Home() {
+//   return (
+//     <main className="flex bg-white min-h-screen flex-col">
+//       <Hero/>
+
+//       {/* FAQ ACCORDIAN ON MAIN PAGE */}
+//       <div style={{ width: '500px', margin: '0 auto', padding: '20px' }}>
+//         {faqs.map((faq, index) => (
+//           <Accordion key={index} title={faq.title} contents={faq.contents} />
+//         ))}
+//       </div>
+
+
+//       {/* LIBRARIES ON MAIN PAGE */}
+//       <div className="w-full min-h-[400px]">
+//         <div className="text-center py-10 px-4">
+//           <h1 className="text-4xl sm:text-5xl pb-16 sm:pb-24">Our Libraries</h1>
+//           <div className="grid grid-cols-2">
+//             <div>
+//               <h1 className="text-3xl sm:text-4xl">ZK Email Libraries</h1>
+//               <p className="w-1/2 justify-center justify-self-center mx-auto py-6 text-[11px]  sm:text-[14px] h-[190px] md:h-[120px]">
+//                 We have several repos within the ZK Email ecosystem. Developers can use these libraries to build their own custom email verification circuits.
+//               </p>
+//               <PopOut cards={PopOutZKEmailLibraryCards} toggleName="ZK Email Specific Libraries" />
+//             </div>
+//             <div>
+//               <h1 className="text-3xl sm:text-4xl">ZK Libraries</h1>
+//               <p className="w-1/2 justify-center justify-self-center mx-auto py-6 text-[11px]  sm:text-[14px] h-[190px]  md:h-[120px]">
+//                 While developing ZK Email we also built these libraries for general use, that can be applied outside of the ZK Email ecosystem
+//               </p>
+//               <PopOut cards={PopOutZKLibraryCards} toggleName='General ZK Libraries'/>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+
+//       {/* VIDEO CARASOLE ON MAIN PAGE */}
+//     <div className="w-full py-24">
+//       <div className=" text-center py-10">
+//         <h1 className="text-5xl">Upcoming & Past Talks</h1>
+//         <p className="py-4">Meet the team <span className="font-bold">Aayush, Sora, Saleel, Wataru, Aditya and Elo</span>  <br/>
+//           at one of our upcoming talks or message us if you have any questions!</p>
+//       </div>
+//       <div className="bg-blue-500 h-[430px] w-full mb-10">
+//         <VideoCarousel />
+//       </div>
+//     </div>
+
+//     </main>
+//   );
+// }
+
+
 import Hero from './hero/hero'
 import Accordion from '../components/Accordion';
 import PopOut from '../components/PopOut';
 import VideoCarousel from '../components/VideoCarousel';
-
-
-
-
-
 
 const faqs = [
   {
@@ -35,27 +127,24 @@ let PopOutZKEmailLibraryCards = [
   { label: "ZK JWTs", url: "https://example.com/3" },
   { label: "Regex Registry", url: "https://example.com/3" },
   { label: "Ether Email Auth", url: "https://example.com/3" },
-  ];
+];
 
-
-
-
-  
 export default function Home() {
   return (
     <main className="flex bg-white min-h-screen flex-col">
       <Hero/>
 
-      {/* FAQ ACCORDIAN ON MAIN PAGE */}
-      <div style={{ width: '500px', margin: '0 auto', padding: '20px' }}>
-        {faqs.map((faq, index) => (
-          <Accordion key={index} title={faq.title} contents={faq.contents} />
-        ))}
+      {/* FAQ ACCORDION ON MAIN PAGE */}
+      <div className='z-50 bg-white w-full min-h-[200px]'>
+        <div className="relative" style={{ width: '500px', margin: '0 auto', padding: '20px', zIndex:'100' }}>
+          {faqs.map((faq, index) => (
+            <Accordion key={index} title={faq.title} contents={faq.contents} />
+          ))}
+        </div>
       </div>
 
-
       {/* LIBRARIES ON MAIN PAGE */}
-      <div className="w-full min-h-[400px]">
+      <div className="w-full min-h-[400px] relative z-10">
         <div className="text-center py-10 px-4">
           <h1 className="text-4xl sm:text-5xl pb-16 sm:pb-24">Our Libraries</h1>
           <div className="grid grid-cols-2">
@@ -77,19 +166,18 @@ export default function Home() {
         </div>
       </div>
 
-
-      {/* VIDEO CARASOLE ON MAIN PAGE */}
-    <div className="w-full py-24">
-      <div className=" text-center py-10">
-        <h1 className="text-5xl">Upcoming & Past Talks</h1>
-        <p className="py-4">Meet the team <span className="font-bold">Aayush, Sora, Saleel, Wataru, Aditya and Elo</span>  <br/>
-          at one of our upcoming talks or message us if you have any questions!</p>
+      {/* VIDEO CAROUSEL ON MAIN PAGE */}
+      <div className="w-full py-24">
+        <div className="text-center py-10">
+          <h1 className="text-5xl">Upcoming & Past Talks</h1>
+          <p className="py-4">Meet the team <span className="font-bold">Aayush, Sora, Saleel, Wataru, Aditya and Elo</span>  <br/>
+            at one of our upcoming talks or message us if you have any questions!</p>
+        </div>
+        <div className="bg-blue-500 h-[430px] w-full mb-10">
+          <VideoCarousel />
+        </div>
       </div>
-      <div className="bg-blue-500 h-[430px] w-full mb-10">
-        <VideoCarousel />
-      </div>
-    </div>
-
     </main>
   );
 }
+
