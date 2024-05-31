12e6280ce7c7f5099475d3801805e59432619acd
Eesha <eeshaulh@gmail.com>
2024-05-30 20:50:59 -0400

    added a title/description section for each blog page

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -1,23 +1,62 @@
-import { getPostBySlug } from '../../lib/index'
+// import { Typography } from '@mui/material'
+// import { getPostBySlug } from '../../lib/index'
+
+// const getPageContent = async slug => {
+//   const { meta, content } = await getPostBySlug(slug)
+//   return { meta, content }
+// }
+
+// export async function generateMetadata({ params }) {
+//   const { meta } = await getPageContent(params.slug)
+//   return { title: meta.title }
+// }
+
+// const Page = async ({ params }) => {
+//   const { content } = await getPageContent(params.slug)
+
+//   return (
+//     <section className='py-24 px-[15%]'>
+//       <Typography sx={{color:'black'}}>  </Typography>
+//       <article className='prose lg:prose-xl'>{content}</article>
+//     </section>
+//   )
+// }
+
+// export default Page
+
+
+import { Typography, Box } from '@mui/material';
+import { getPostBySlug } from '../../lib/index';
 
 const getPageContent = async slug => {
-  const { meta, content } = await getPostBySlug(slug)
-  return { meta, content }
-}
+  const { meta, content } = await getPostBySlug(slug);
+  return { meta, content };
+};
 
 export async function generateMetadata({ params }) {
-  const { meta } = await getPageContent(params.slug)
-  return { title: meta.title }
+  const { meta } = await getPageContent(params.slug);
+  return { title: meta.title };
 }
 
 const Page = async ({ params }) => {
-  const { content } = await getPageContent(params.slug)
+  const { meta, content } = await getPageContent(params.slug);
 
   return (
     <section className='py-24 px-[15%]'>
+      <Box mb={4}>
+        <Typography variant="h1" component="h1" sx={{ color: 'black' }}>
+          {meta.title}
+        </Typography>
+        <Typography variant="body1" sx={{ color: 'grey.600' }}>
+          {new Date(meta.date).toLocaleDateString()} | {meta.category}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'grey.800', mt: 2 }}>
+          {meta.description}
+        </Typography>
+      </Box>
       <article className='prose lg:prose-xl'>{content}</article>
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
