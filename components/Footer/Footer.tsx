8ff1bc54e52ea84d48fd3288344bb9415a604c25
Eesha <eeshaulh@gmail.com>
2024-06-10 16:00:23 -0400

    change footer and header onhover

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,196 @@
+// 'use client';
+
+// import React, { useState, useEffect } from 'react';
+// import { Box, Grid, Typography } from '@mui/material';
+// import XIcon from '@mui/icons-material/X';
+// import TelegramIcon from '@mui/icons-material/Telegram';
+// import YouTubeIcon from '@mui/icons-material/YouTube';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import Image from 'next/image';
+// import IconLink from '../IconLink/IconLink';
+// import backgroundFooter from '../../public/backgroundFooter.svg';
+// import backgroundFooter2 from '../../public/backgroundFooter2.svg'; 
+// import zkEmailFooter from '../../public/zkEmailFooter.svg'; 
+// import Link from 'next/link';
+// import DiscordIcon from '../DiscordIcon/DiscordIcon'; // Import the DiscordIcon component
+// import { animate, motion, useAnimation } from 'framer-motion';
+// import { useInView } from 'react-intersection-observer';
+
+// const Footer = () => {
+
+//   const fadeInAnimationVariants = {
+//     initial: {
+//       opacity: 0,
+//       y: 100,
+//     },
+//     animate: {
+//       opacity: 1,
+//       y: 0,
+//       transition: {
+//         duration: 0.5, // Adjust the duration to slow down the animation
+//         ease: 'easeInOut' // Optional, you can change the easing function as needed
+//       }
+//     }
+//   };
+  
+//   const backgrounds = [backgroundFooter, backgroundFooter2];
+//   const [currentBackground, setCurrentBackground] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentBackground(prev => (prev + 1) % backgrounds.length);
+//     }, 500);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <Box
+//       sx={{
+//         width: '100%',
+//         backgroundImage: `url(${backgrounds[currentBackground].src})`,
+//         backgroundRepeat: 'no-repeat',
+//         backgroundSize: 'cover',
+//         backgroundPosition: 'center',
+//         transition: 'background-image', // smooth transition for background image change
+//         backgroundColor: 'white', // ensure background color doesn't override background image
+//       }}
+//     >
+//       <Box
+//         component="footer"
+//         sx={{
+//           pt: { xs: 16, sm: 28 },
+//           textAlign: 'center',
+//           marginTop: 'auto',
+//           backgroundColor: 'transparent', // ensure background color doesn't override background image
+//           fontFamily: ''
+//         }}
+//       >
+//         <Box
+//           sx={{
+//             px: {
+//               xs: 3,
+//               md: 6,
+//             },
+//             height: "100%",
+//             display: "flex",
+//             flexDirection: "column",
+//             justifyContent: "space-between"
+//           }}
+//         >
+//           {/* FOOTER TAGLINE */}
+//           <Grid container spacing={2}>
+//             <Grid item xs={3} sm={4} justifyContent="flex-start">
+//               <Typography color="white" textAlign="left" sx={{fontSize:{xs:"10px", sm:"15px", md:"20px"}}}>
+//                 transfer truth with the freedom of privacy
+//               </Typography>
+//             </Grid>
+
+//             {/* FOOTER SITE LINKS */}
+//             <Grid item xs={9} sm={8} justifyContent="flex-end">
+//               <Grid container spacing={6} justifyContent="flex-end">
+//                 <Grid item>
+//                   <Typography variant="h6" component="h3" color="white" fontWeight="bold" pb={2} textAlign="center" sx={{fontSize:{xs:'10px', md:'14px'}}}>
+//                     DEVELOPERS
+//                   </Typography>
+//                   <Grid container direction="column" spacing={1} textAlign="center" color="text.secondary">
+//                     <Grid item>
+//                       <Link href="https://zkemail.gitbook.io/zk-email" color="inherit" target="_blank">
+//                         <Typography variant='footerLink'>
+//                           Docs
+//                         </Typography>
+//                       </Link>
+//                     </Grid>
+//                     <Grid item>
+//                       <Link href="/projects" target="_blank" color="inherit">
+//                         <Typography variant='footerLink'>
+//                           Projects
+//                         </Typography>
+//                       </Link>
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+
+//                 <Grid item>
+//                   <Typography variant="h6" component="h3" color="white" fontWeight="bold" pb={2} textAlign="center" sx={{fontSize:{xs:'10px', md:'14px'}}}>
+//                     COMMUNITY
+//                   </Typography>
+//                   <Grid container direction="column" spacing={1} textAlign="center" color="text.secondary">
+//                     <Grid item>
+//                       <Link href="/blog" target="_blank" color="inherit" >
+//                         <Typography variant='footerLink'>
+//                           Blog
+//                         </Typography>
+//                       </Link>
+//                     </Grid>
+//                     <Grid item>
+//                       <Link href="/projects" target="_blank" color="inherit">
+//                         <Typography variant='footerLink'>
+//                           Learn
+//                         </Typography>
+//                       </Link>
+//                     </Grid>
+//                     <Grid item>
+//                       <Link href="/projects" target="_blank" color="inherit">
+//                         <Typography variant='footerLink'>
+//                           Partner
+//                         </Typography>
+//                       </Link>
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+
+
+//                 {/* FOOTER SOCIAL ICONS  */}
+//                 <Grid item>
+//                   <Grid container justifyContent="center" spacing={2}>
+//                     <Grid item>
+//                       <IconLink target={false} href="https://twitter.com/zkemail?lang=en" IconComponent={XIcon} />
+//                     </Grid>
+//                     <Grid item>
+//                       <IconLink target={false} href="https://t.me/zkemail" IconComponent={TelegramIcon} />
+//                     </Grid>
+//                     <Grid item>
+//                       <IconLink target={false} href="https://discord.com/invite/yourlink" IconComponent={DiscordIcon} />
+//                     </Grid>
+//                     <Grid item>
+//                       <IconLink target={false} href="https://www.youtube.com/yourchannel" IconComponent={YouTubeIcon} />
+//                     </Grid>
+//                     <Grid item>
+//                       <IconLink target={false} href="https://github.com/zkemail" IconComponent={GitHubIcon} />
+//                     </Grid>
+//                   </Grid>
+//                 </Grid>
+//               </Grid>
+//             </Grid>
+//           </Grid>
+
+
+//           {/* BOTTOM FOOTER ZK EMAIL BIG TEXT*/}
+//           <Box sx={{ marginTop: 'auto' }}>
+//             <motion.div
+//               variants={fadeInAnimationVariants}
+//               initial='initial'
+//               whileInView='animate'
+//               viewport={{once: true}}
+//             >
+//               <Image
+//                 src={zkEmailFooter}
+//                 alt='checkbox'
+//                 className='w-full h-full fill-white pt-24'
+//               />
+//             </motion.div>
+//           </Box>
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default Footer;
+
+
+
 'use client';
 
 import React, { useState, useEffect } from 'react';
@@ -13,10 +206,11 @@ import backgroundFooter2 from '../../public/backgroundFooter2.svg';
 import zkEmailFooter from '../../public/zkEmailFooter.svg'; 
 import Link from 'next/link';
 import DiscordIcon from '../DiscordIcon/DiscordIcon'; // Import the DiscordIcon component
-import { animate, motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import { useTheme } from '@mui/material/styles';
 
 const Footer = () => {
+  const theme = useTheme();
 
   const fadeInAnimationVariants = {
     initial: {
@@ -96,14 +290,60 @@ const Footer = () => {
                   <Grid container direction="column" spacing={1} textAlign="center" color="text.secondary">
                     <Grid item>
                       <Link href="https://zkemail.gitbook.io/zk-email" color="inherit" target="_blank">
-                        <Typography variant='footerLink'>
+                        <Typography 
+                          variant='footerLink'
+                          sx={{
+                            position: 'relative',
+                            '&:hover': { 
+                              backgroundColor: 'transparent', // No background color on hover
+                              '&::after': {
+                                transform: 'scaleX(1)',
+                              }
+                            }, 
+                            '&::after': {
+                              content: '""',
+                              position: 'absolute',
+                              bottom: 0,
+                              left: 0,
+                              width: '100%',
+                              height: '2px',
+                              backgroundColor: theme.palette.secondary.main, // Underline color
+                              transform: 'scaleX(0)',
+                              transformOrigin: 'bottom right',
+                              transition: 'transform 0.3s ease-out',
+                            },
+                          }}
+                        >
                           Docs
                         </Typography>
                       </Link>
                     </Grid>
                     <Grid item>
                       <Link href="/projects" target="_blank" color="inherit">
-                        <Typography variant='footerLink'>
+                        <Typography 
+                          variant='footerLink'
+                          sx={{
+                            position: 'relative',
+                            '&:hover': { 
+                              backgroundColor: 'transparent', // No background color on hover
+                              '&::after': {
+                                transform: 'scaleX(1)',
+                              }
+                            }, 
+                            '&::after': {
+                              content: '""',
+                              position: 'absolute',
+                              bottom: 0,
+                              left: 0,
+                              width: '100%',
+                              height: '2px',
+                              backgroundColor: theme.palette.secondary.main, // Underline color
+                              transform: 'scaleX(0)',
+                              transformOrigin: 'bottom right',
+                              transition: 'transform 0.3s ease-out',
+                            },
+                          }}
+                        >
                           Projects
                         </Typography>
                       </Link>
@@ -118,28 +358,96 @@ const Footer = () => {
                   <Grid container direction="column" spacing={1} textAlign="center" color="text.secondary">
                     <Grid item>
                       <Link href="/blog" target="_blank" color="inherit" >
-                        <Typography variant='footerLink'>
+                        <Typography 
+                          variant='footerLink'
+                          sx={{
+                            position: 'relative',
+                            '&:hover': { 
+                              backgroundColor: 'transparent', // No background color on hover
+                              '&::after': {
+                                transform: 'scaleX(1)',
+                              }
+                            }, 
+                            '&::after': {
+                              content: '""',
+                              position: 'absolute',
+                              bottom: 0,
+                              left: 0,
+                              width: '100%',
+                              height: '2px',
+                              backgroundColor: theme.palette.secondary.main, // Underline color
+                              transform: 'scaleX(0)',
+                              transformOrigin: 'bottom right',
+                              transition: 'transform 0.3s ease-out',
+                            },
+                          }}
+                        >
                           Blog
                         </Typography>
                       </Link>
                     </Grid>
                     <Grid item>
                       <Link href="/projects" target="_blank" color="inherit">
-                        <Typography variant='footerLink'>
+                        <Typography 
+                          variant='footerLink'
+                          sx={{
+                            position: 'relative',
+                            '&:hover': { 
+                              backgroundColor: 'transparent', // No background color on hover
+                              '&::after': {
+                                transform: 'scaleX(1)',
+                              }
+                            }, 
+                            '&::after': {
+                              content: '""',
+                              position: 'absolute',
+                              bottom: 0,
+                              left: 0,
+                              width: '100%',
+                              height: '2px',
+                              backgroundColor: theme.palette.secondary.main, // Underline color
+                              transform: 'scaleX(0)',
+                              transformOrigin: 'bottom right',
+                              transition: 'transform 0.3s ease-out',
+                            },
+                          }}
+                        >
                           Learn
                         </Typography>
                       </Link>
                     </Grid>
                     <Grid item>
-                      <Link href="/projects" target="_blank" color="inherit">
-                        <Typography variant='footerLink'>
+                      <Link href="https://t.me/zkemail" target="_blank" color="inherit">
+                        <Typography 
+                          variant='footerLink'
+                          sx={{
+                            position: 'relative',
+                            '&:hover': { 
+                              backgroundColor: 'transparent', // No background color on hover
+                              '&::after': {
+                                transform: 'scaleX(1)',
+                              }
+                            }, 
+                            '&::after': {
+                              content: '""',
+                              position: 'absolute',
+                              bottom: 0,
+                              left: 0,
+                              width: '100%',
+                              height: '2px',
+                              backgroundColor: theme.palette.secondary.main, // Underline color
+                              transform: 'scaleX(0)',
+                              transformOrigin: 'bottom right',
+                              transition: 'transform 0.3s ease-out',
+                            },
+                          }}
+                        >
                           Partner
                         </Typography>
                       </Link>
                     </Grid>
                   </Grid>
                 </Grid>
-
 
                 {/* FOOTER SOCIAL ICONS  */}
                 <Grid item>
@@ -150,9 +458,9 @@ const Footer = () => {
                     <Grid item>
                       <IconLink target={false} href="https://t.me/zkemail" IconComponent={TelegramIcon} />
                     </Grid>
-                    <Grid item>
+                    {/* <Grid item>
                       <IconLink target={false} href="https://discord.com/invite/yourlink" IconComponent={DiscordIcon} />
-                    </Grid>
+                    </Grid> */}
                     <Grid item>
                       <IconLink target={false} href="https://www.youtube.com/yourchannel" IconComponent={YouTubeIcon} />
                     </Grid>
@@ -164,7 +472,6 @@ const Footer = () => {
               </Grid>
             </Grid>
           </Grid>
-
 
           {/* BOTTOM FOOTER ZK EMAIL BIG TEXT*/}
           <Box sx={{ marginTop: 'auto' }}>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,106 @@
+// 'use client';
+// import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Button from '@mui/material/Button';
+// import Container from '@mui/material/Container';
+// import Link from 'next/link';
+// import IconButton from '@mui/material/IconButton';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import Brightness4Icon from '@mui/icons-material/Brightness4';
+// import { Typography } from '@mui/material';
+// import { AppBarProps } from '../../types'; 
+// import { useTheme } from '@mui/material/styles';
+// import IconLink from '../../components/IconLink/IconLink';
+// import ZKEIcon from '../../components/ZKEIcon/ZKEIcon'; // Import the ZKEIcon component
+// import { color } from 'framer-motion';
+
+
+// const ResponsiveAppBar: React.FC<AppBarProps> = ({ title, pages }) => {
+//   const theme = useTheme();
+
+//   return (
+//     <AppBar position="static" 
+//       sx={{ backgroundColor: 'white', paddingY: '22px', boxShadow: '0px 1px 10.8px rgba(0, 0, 0, 0.05)', zIndex: '10' 
+//     }}>
+//       <Container
+//         sx={{ 
+//           backgroundColor: 'white', 
+//           boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.09)', 
+//           borderRadius: '5px',
+//           maxWidth: {xs: '80%', md:'50%'}
+//         }}>
+//         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', padding: '0 !important'}}>
+//             <IconButton
+//              href='/'
+//               sx={{
+//                 color: 'black',
+//                 padding: { xs: '4px', sm: '6px', md: '8px' }, // Adjust padding based on screen size
+//                 '& .MuiSvgIcon-root': {
+//                   fontSize: { xs: '16px', sm: '20px', md: '24px' } // Adjust icon size based on screen size
+//                 }
+//               }}
+//               aria-label="GitHub link"
+//             ><ZKEIcon/></IconButton>
+
+
+//           <Box sx={{ display: 'flex', gap: {xs: 0.5, sm: 1, md:2}}}>
+//             {pages.map((page) => (
+//               <Link href={page.link} key={page.label} passHref>
+//                 <Button sx={{ 
+//                   color: 'black', 
+//                   textTransform: 'capitalize', 
+//                   fontSize: { xs: '0.6rem', sm:'0.9rem', md: '1 rem'}, 
+//                   '&:hover': { backgroundColor: theme.palette.secondary.main}, // swapped rgba(0, 0, 0, 0.04) with yellow
+//                   paddingX: { xs: '1px', md: '6px' },
+//                   padding: { xs: '2px 4px', md: '6px 16px' }, 
+//                   minWidth: 'auto', 
+//                 }}>
+//                   {page.label}
+//                 </Button>
+//               </Link>
+//             ))}
+//           </Box>
+//           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+//             <IconButton
+//              href='https://github.com/zkemail/'
+//              target='_blank'
+//               sx={{
+//                 color: 'black',
+//                 padding: { xs: '4px', sm: '6px', md: '8px' }, // Adjust padding based on screen size
+//                 '& .MuiSvgIcon-root': {
+//                   fontSize: { xs: '16px', sm: '20px', md: '24px' } // Adjust icon size based on screen size
+//                 }
+//               }}
+//               aria-label="GitHub link"
+//             >
+
+//             <GitHubIcon/>
+//             </IconButton>
+
+//             {/* <IconButton
+//               sx={{
+//                 color: 'black',
+//                 padding: { xs: '4px', sm: '6px', md: '8px' }, // Adjust padding based on screen size
+//                 '& .MuiSvgIcon-root': {
+//                   fontSize: { xs: '12px', sm: '20px', md: '24px' } // Adjust icon size based on screen size
+//                 }
+//               }}
+//               aria-label="Toggle theme"
+//             >
+//               <Brightness4Icon />
+//             </IconButton> */}
+//           </Box>
+//         </Toolbar>
+//       </Container>
+//     </AppBar>
+//   );
+// }
+
+// export default ResponsiveAppBar;
+
+
 'use client';
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -14,8 +117,6 @@ import { AppBarProps } from '../../types';
 import { useTheme } from '@mui/material/styles';
 import IconLink from '../../components/IconLink/IconLink';
 import ZKEIcon from '../../components/ZKEIcon/ZKEIcon'; // Import the ZKEIcon component
-import { color } from 'framer-motion';
-
 
 const ResponsiveAppBar: React.FC<AppBarProps> = ({ title, pages }) => {
   const theme = useTheme();
@@ -28,7 +129,7 @@ const ResponsiveAppBar: React.FC<AppBarProps> = ({ title, pages }) => {
         sx={{ 
           backgroundColor: 'white', 
           boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.09)', 
-          borderRadius: '5px',
+          borderRadius: '15px',
           maxWidth: {xs: '80%', md:'50%'}
         }}>
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', padding: '0 !important'}}>
@@ -51,11 +152,29 @@ const ResponsiveAppBar: React.FC<AppBarProps> = ({ title, pages }) => {
                 <Button sx={{ 
                   color: 'black', 
                   textTransform: 'capitalize', 
-                  fontSize: { xs: '0.6rem', sm:'0.9rem', md: '1 rem'}, 
-                  '&:hover': { backgroundColor: theme.palette.secondary.main}, // swapped rgba(0, 0, 0, 0.04) with yellow
+                  fontSize: { xs: '0.6rem', sm:'0.9rem', md: '1rem'}, 
+                  position: 'relative', // Add this line
+                  '&:hover': { 
+                    backgroundColor: 'transparent', // No background color on hover
+                    '&::after': {
+                      transform: 'scaleX(1)',
+                    }
+                  }, 
                   paddingX: { xs: '1px', md: '6px' },
                   padding: { xs: '2px 4px', md: '6px 16px' }, 
-                  minWidth: 'auto', 
+                  minWidth: 'auto',
+                  '&::after': {
+                    content: '""',
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '2px',
+                    backgroundColor: theme.palette.secondary.main, // Underline color
+                    transform: 'scaleX(0)',
+                    transformOrigin: 'bottom right',
+                    transition: 'transform 0.3s ease-out',
+                  },
                 }}>
                   {page.label}
                 </Button>
@@ -76,22 +195,8 @@ const ResponsiveAppBar: React.FC<AppBarProps> = ({ title, pages }) => {
               aria-label="GitHub link"
             >
 
-            {/* NEED to link these icons https://github.com/zkemail/ */}
             <GitHubIcon/>
             </IconButton>
-
-            {/* <IconButton
-              sx={{
-                color: 'black',
-                padding: { xs: '4px', sm: '6px', md: '8px' }, // Adjust padding based on screen size
-                '& .MuiSvgIcon-root': {
-                  fontSize: { xs: '12px', sm: '20px', md: '24px' } // Adjust icon size based on screen size
-                }
-              }}
-              aria-label="Toggle theme"
-            >
-              <Brightness4Icon />
-            </IconButton> */}
           </Box>
         </Toolbar>
       </Container>
