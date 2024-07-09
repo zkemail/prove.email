304d412da2095721114612249e956efc0ef837f1
Eesha <eeshaulh@gmail.com>
2024-07-09 08:46:10 -0400

    change interval

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -241,7 +241,7 @@ export default function Hero() {
   useEffect(() => {
     const interval = setInterval(() => {
       setBackgroundIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
-    }, 20);
+    }, 150);
     return () => clearInterval(interval);
   }, []);
 
