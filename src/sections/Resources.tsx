27ea37ae9e7fa8b0ddd06882ad871f760a66e090
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-10-19 05:40:36 +0700

    ui: update button components

--- src/sections/Resources.tsx
+++ src/sections/Resources.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useWindowWidth } from "../utils";
 import BlueDiamond from "../assets/BlueDiamond.svg";
 import RedDiamond from "../assets/RedDiamond.svg";
 import GreenDiamond from "../assets/GreenDiamond.svg";
+import Button from "../components/Button";
+import CaretRight from "../assets/CaretRight.svg";
+import CaretLeft from "../assets/CaretLeft.svg";
 
 const RESOURCE_CONFIG = {
   Talk: {
@@ -166,14 +169,6 @@ const Resources = () => {
     }
   };
 
-  console.log(
-    currentIndex >= resources.length - visibleCards,
-    currentIndex === 0,
-    currentIndex,
-    resources,
-    visibleCards
-  );
-
   return (
     <div>
       <h1 className="h3">Resources</h1>
@@ -218,6 +213,12 @@ const Resources = () => {
                 className="shadow-md overflow-hidden cursor-pointer relative h-72 transform transition-transform duration-300 ease-in-out"
                 onClick={() => window.open(resource.url, "_blank")}
               >
+                <img
+                  src={resource.thumbnail}
+                  alt={resource.title}
+                  className="absolute top-0 left-0 w-full h-3/5 object-cover opacity-50 transition-opacity duration-300 ease-in-out hover:opacity-100"
+                />
+
                 <div
                   className="p-4 grid grid-cols-2 bg-black relative border-solid border-[1px] border-[#272727]"
                   style={{
@@ -282,45 +283,20 @@ const Resources = () => {
       </div>
       {!isSmallScreen && (
         <div className="flex justify-center space-x-4 mt-12">
-          <button
+          <Button
+            startIcon={CaretLeft}
             onClick={handlePrevious}
             disabled={currentIndex === 0}
-            className="flex flex-row justify-between items-center gap-1 border-solid border-[2px] border-[#3B3B3B] rounded-[12px] px-[16px] py-[5px] bg-[#1C1C1C] font-semibold"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              fill="none"
-            >
-              <path
-                d="M4.06614 6.69064L8.44114 2.31564C8.48179 2.27499 8.53005 2.24275 8.58316 2.22075C8.63626 2.19875 8.69319 2.18742 8.75067 2.18742C8.80816 2.18742 8.86508 2.19875 8.91819 2.22075C8.9713 2.24275 9.01956 2.27499 9.0602 2.31564C9.10085 2.35629 9.1331 2.40454 9.15509 2.45765C9.17709 2.51076 9.18842 2.56768 9.18842 2.62517C9.18842 2.68265 9.17709 2.73958 9.15509 2.79269C9.1331 2.84579 9.10085 2.89405 9.0602 2.9347L4.99419 7.00017L9.0602 11.0656C9.1423 11.1477 9.18842 11.2591 9.18842 11.3752C9.18842 11.4913 9.1423 11.6026 9.0602 11.6847C8.97811 11.7668 8.86677 11.8129 8.75067 11.8129C8.63458 11.8129 8.52323 11.7668 8.44114 11.6847L4.06614 7.3097C4.02546 7.26907 3.99319 7.22082 3.97118 7.16771C3.94916 7.11459 3.93783 7.05766 3.93783 7.00017C3.93783 6.94267 3.94916 6.88574 3.97118 6.83263C3.99319 6.77952 4.02546 6.73127 4.06614 6.69064Z"
-                fill="#3B3B3B"
-              />
-            </svg>
             Previous
-          </button>
-          <button
+          </Button>
+          <Button
+            endIcon={CaretRight}
             onClick={handleNext}
-            // disabled={currentIndex >= resources.length - visibleCards}
-            disabled={false}
-            className="flex flex-row justify-between items-center gap-1 border-solid border-[2px] border-[#3B3B3B] rounded-[12px] px-[16px] py-[5px] bg-[#1C1C1C] font-semibold"
+            disabled={currentIndex >= resources.length - getVisibleCards()}
           >
             Next
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              fill="none"
-            >
-              <path
-                d="M9.93389 7.30936L5.55889 11.6844C5.51824 11.725 5.46998 11.7573 5.41688 11.7793C5.36377 11.8013 5.30684 11.8126 5.24936 11.8126C5.19187 11.8126 5.13495 11.8013 5.08184 11.7793C5.02873 11.7573 4.98048 11.725 4.93983 11.6844C4.89918 11.6437 4.86693 11.5955 4.84494 11.5423C4.82294 11.4892 4.81161 11.4323 4.81161 11.3748C4.81161 11.3173 4.82294 11.2604 4.84494 11.2073C4.86693 11.1542 4.89918 11.1059 4.93983 11.0653L9.00584 6.99983L4.93983 2.93436C4.85773 2.85227 4.81161 2.74093 4.81161 2.62483C4.81161 2.50873 4.85773 2.39739 4.93983 2.3153C5.02192 2.23321 5.13326 2.18709 5.24936 2.18709C5.36545 2.18709 5.4768 2.23321 5.55889 2.3153L9.93389 6.6903C9.97457 6.73093 10.0068 6.77918 10.0289 6.83229C10.0509 6.88541 10.0622 6.94234 10.0622 6.99983C10.0622 7.05733 10.0509 7.11426 10.0289 7.16737C10.0068 7.22048 9.97457 7.26873 9.93389 7.30936Z"
-                fill="#F5F3EF"
-              />
-            </svg>
-          </button>
+          </Button>
         </div>
       )}
     </div>
