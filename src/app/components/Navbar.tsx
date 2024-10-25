55a8e88b143c9ce7dedb50a15c0e3ee9a73ec62f
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-10-25 12:44:33 +0700

    ui: Update footer icons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,26 +1,57 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [hoveredSection, setHoveredSection] = useState<string | null>(null);
+
+  const handleMouseEnter = (section: string) => {
+    setHoveredSection(section);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredSection(null);
+  };
+  console.log(hoveredSection);
   return (
     <>
-      <nav className="navbar container-width navbar-desktop">
-        <Link href="/">
-          <img src={"/assets/Logo.svg"} alt="zkemail-logo" />
-        </Link>
-        <div style={{ display: "flex", gap: 24 }}>
-          <p className="body1" style={{ display: "flex", gap: 4 }}>
-            Features <img src={"/assets/CaretDown.svg"} alt="caret-down-icon" />
-          </p>
-          <Link href="/blogs">
-            <p className="body1" style={{color: '#F5F3EF'}}>Blog</p>
+      <nav className="navbar container-width navbar-desktop relative flex flex-col">
+        <div className=" w-full flex flex-row justify-between">
+          <Link href="/">
+            <img src={"/assets/Logo.svg"} alt="zkemail-logo" />
           </Link>
-          <Link href="https://zkemail.gitbook.io/zk-email" target="_blank">
-            <p className="body1" style={{color: '#F5F3EF'}}>Documentation</p>
-          </Link>
+          <div style={{ display: "flex", gap: 24 }}>
+            <div
+              onMouseEnter={() => handleMouseEnter("blog")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <p className="body1" style={{ color: "#F5F3EF" }}>
+                Blog
+              </p>
+            </div>
+
+            <div
+              onMouseEnter={() => handleMouseEnter("documentation")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <p className="body1" style={{ color: "#F5F3EF" }}>
+                Documentation
+              </p>
+            </div>
+          </div>
         </div>
+        {hoveredSection && (
+          <div
+            className="w-full"
+            onMouseEnter={() => handleMouseEnter("expandedSection")}
+            onMouseLeave={handleMouseLeave}
+          >
+            tete
+          </div>
+        )}
       </nav>
+
       <nav className="navbar container-width navbar-mobile">
         <Link href="/">
           <img src={"/assets/Logo.svg"} alt="zkemail-logo" />
